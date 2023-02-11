2a1e8f59a29af161ece08511cb4c62d120ec7d2d
Anton Chepko <choptonys@gmail.com>
2023-02-12 00:44:35 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,63 @@
+# Frontend Mentor - Product preview card component solution
+
+This is a solution to the [Product preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa).
+
+## Table of contents
+
+- [Frontend Mentor - Product preview card component solution](#frontend-mentor---product-preview-card-component-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover and focus states for interactive elements
+
+### Screenshot
+
+![](screenshots/product-prev-desktop-screen.png)
+![](screenshots/product-prev-mobile-screen.png)
+
+### Links
+
+- Solution URL: [Solution](https://github.com/achepko/P1-product-preview-card-component)
+- Live Site URL: [Live](https://achepko.github.io/P1-product-preview-card-component/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- IntelliJ IDEA 2022.3
+
+### What I learned
+
+I learned how to change icon of tab in browser.
+```html
+
+<link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+
+```
+
+### Useful resources
+
+- [Flexbox](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox) - Basic concepts of flexbox
+- [Favicon](https://developer.mozilla.org/en-US/docs/Glossary/Favicon) - Information of favicon
+
+## Author
+- Frontend Mentor - [@achepko](https://www.frontendmentor.io/profile/achepko)
+
